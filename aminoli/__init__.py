3a5668959a507f535f304bc8260cc46719c73f1e
LucasAlgerDay <88118098+LucasAlgerDay@users.noreply.github.com>
2023-03-10 12:13:56 -0600

    Add files via upload

--- aminoli/__init__.py
+++ aminoli/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'Amino.li'
 __author__ = 'Lucas'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023'
-__version__ = '0.4'
+__version__ = '0.5'
 
 from .acm import ACM
 from .client import Client
@@ -13,8 +13,8 @@ from .socket import Callbacks, SocketHandler
 from requests import get
 from json import loads
 
-# __newest__ = loads(get("https://pypi.org/project/amino.li/").text)["info"]["version"]
+__newest__ = loads(get("https://pypi.org/pypi/amino.li/json").text)["info"]["version"]
 
-# if __version__ != __newest__:
-#     print(f"New version available: {__newest__} (Using {__version__})")
-#     print("Talk in discord like 「𝑴𝑶𝑵𝑺𝑻𝑬𝑹」#4545")
+if __version__ != __newest__:
+    print(f"New version available: {__newest__} (Using {__version__})")
+    print("Talk in discord like 「𝑴𝑶𝑵𝑺𝑻𝑬𝑹」#4545")
